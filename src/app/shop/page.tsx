4e273bf5fe912d3bbd19afa54261befc77c58884
Rafsan-Ratul <rafsan.ratul.mians@gmail.com>
2025-08-16 15:54:43 +0600

    ✨ Feat: Desktop Header design done

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 import React from "react";
 import MobileHeader from "../components/common/MobileHeader";
+import DesktopHeader from "../components/common/DesktopHeader";
 
 const Shop = () => {
   return (
     <>
       <MobileHeader />
+      <DesktopHeader />
+      <p>This is shop page</p>
     </>
   );
 };
