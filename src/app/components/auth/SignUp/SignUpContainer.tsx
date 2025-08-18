8dda517a83ef01f89e7fda0b551d004ba502c50a
shahnawroz <sh.nawrose@gmail.com>
2025-08-18 13:41:33 +0600

    :arrow_up: Update: header added into login and signup page

--- src/app/components/auth/SignUp/SignUpContainer.tsx
+++ src/app/components/auth/SignUp/SignUpContainer.tsx
@@ -3,12 +3,15 @@
 import { SignUpForm } from "./SignUpForm";
 import { DesktopLayout } from "../../ui/sheiAuthLayout/AuthDesktop";
 import { MobileLayout } from "../../ui/sheiAuthLayout/AuthMobile";
+import MobileHeader from "../../common/MobileHeader";
+import DesktopHeader from "../../common/DesktopHeader";
 
 export function SignUpContainer() {
   return (
     <>
       {/* Mobile Layout */}
       <div className="block md:hidden">
+        <MobileHeader />
         <MobileLayout>
           <SignUpForm />
         </MobileLayout>
@@ -16,6 +19,7 @@ export function SignUpContainer() {
 
       {/* Desktop Layout */}
       <div className="hidden md:block">
+        <DesktopHeader />
         <DesktopLayout>
           <SignUpForm />
         </DesktopLayout>
