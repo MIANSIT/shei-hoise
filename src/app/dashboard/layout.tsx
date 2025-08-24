06e4821fb0766e02918312d2fd6c1819db493853
shahnawroz <sh.nawrose@gmail.com>
2025-08-24 23:28:42 +0600

    :bug: Fix: add auth gurad for admin panel

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -4,19 +4,21 @@
 import React from "react";
 import Sidebar from "../components/admin/sidebar/Sidebar";
 import Header from "../components/common/Header";
+import ProtectedRoute from "../components/common/ProtectedRoute";
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="min-h-screen flex flex-col bg-black">
-      {/* Header on top */}
-      <Header isAdmin={true} />
+    <ProtectedRoute>
+      <div className="min-h-screen flex flex-col bg-black">
+        {/* Header */}
+        <Header isAdmin={true} />
 
-      {/* Content area */}
-      <div className="flex flex-1">
-        <Sidebar />
-
-        <main className="flex-1 p-4">{children}</main>
+        {/* Sidebar + main content */}
+        <div className="flex flex-1">
+          <Sidebar />
+          <main className="flex-1 p-4">{children}</main>
+        </div>
       </div>
-    </div>
+    </ProtectedRoute>
   );
 }
