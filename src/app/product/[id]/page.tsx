6e5d3246e42865dc12edf25d9f1e762864bfceb1
Rafsan-Ratul <rafsan.ratul.mians@gmail.com>
2025-08-27 05:13:41 +0600

    🐛 Fix: Header fixed in product details page

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,10 +1,11 @@
-"use client"
+"use client";
 import { useParams } from "next/navigation";
 import { dummyProducts } from "../../../lib/store/dummyProducts";
 import MobileHeader from "../../components/common/MobileHeader";
 import DesktopHeader from "../../components/common/DesktopHeader";
 import { useState } from "react";
 import useCartStore from "@/lib/store/cartStore";
+import Header from "../../components/common/Header";
 
 import ProductImage from "../../components/products/singleProduct/ProductImage";
 import ProductTitle from "../../components/products/singleProduct/ProductTitle";
@@ -95,17 +96,8 @@ export default function ProductPage() {
 
   return (
     <>
-      {/* Headers outside of the container */}
-      <div className="block md:hidden">
-        <MobileHeader />
-      </div>
-      <div className="hidden md:block">
-        <DesktopHeader />
-      </div>
-
-      {/* Main page content inside container */}
+      <Header />
       <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-6 md:py-10">
-        {/* Back button */}
         <BackButton href="/shop" label="Back to Products" />
 
         {/* Top section: image + details */}
