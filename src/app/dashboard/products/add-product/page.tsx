4db61a4f84191ef623c94eb4026afa020ee33ec5
shahnawroz <sh.nawrose@gmail.com>
2025-10-02 06:30:36 +0600

    :bug: Fix: redirect after succesful product create

--- src/app/dashboard/products/add-product/page.tsx
+++ src/app/dashboard/products/add-product/page.tsx
@@ -1,13 +1,17 @@
 "use client";
 
 import React, { useRef } from "react";
-import AddProductForm, { AddProductFormRef } from "@/app/components/admin/dashboard/products/addProducts/AddProductForm";
+import { useRouter } from "next/navigation"; // App Router
+import AddProductForm, {
+  AddProductFormRef,
+} from "@/app/components/admin/dashboard/products/addProducts/AddProductForm";
 import { ProductType } from "@/lib/schema/productSchema";
 import { useSheiNotification } from "@/lib/hook/useSheiNotification";
 import { useCurrentUser } from "@/lib/hook/useCurrentUser";
 import { createProduct } from "@/lib/queries/products/createProduct";
 
 export default function AddProductPage() {
+  const router = useRouter();
   const { success, error } = useSheiNotification();
   const { user, loading } = useCurrentUser();
   const formRef = useRef<AddProductFormRef>(null);
@@ -18,10 +22,17 @@ export default function AddProductPage() {
   const handleSubmit = async (product: ProductType) => {
     try {
       await createProduct(product);
-      success(<div>🎉 <b>{product.name}</b> has been added successfully!</div>);
+      success(
+        <div>
+          🎉 <b>{product.name}</b> has been added successfully!
+        </div>
+      );
 
       // Reset the form using ref
       formRef.current?.reset();
+
+      // Redirect to all products page
+      router.push("/dashboard/products");
     } catch (err) {
       console.error(err);
       error("❌ Failed to add product. Please try again.");
