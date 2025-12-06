4688d7a11b34d4c6be9a58544e2d1595c7d4532c
Samin Sharar <samin@valerianfunds.com>
2025-12-06 23:20:19 +0600

    :bug: Build fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,40 +1,40 @@
-import type { Metadata } from "next"
-import { Geist, Geist_Mono } from "next/font/google"
-import "./globals.css"
-import { Toaster } from "./components/ui/sheiSonner/sonner" // Import your custom Toaster
-import { CartProvider } from "@/lib/context/CartContext"
+import type { Metadata } from "next";
+// import { Geist, Geist_Mono } from "next/font/google"
+import "./globals.css";
+import { Toaster } from "./components/ui/sheiSonner/sonner"; // Import your custom Toaster
+import { CartProvider } from "@/lib/context/CartContext";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-})
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-})
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export const metadata: Metadata = {
   title: "Shei Hoise",
   description: "Shei Hoise is an e-commerce powered by MIANS",
-}
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang='en' suppressHydrationWarning>
       <body
         suppressHydrationWarning
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <CartProvider>
-        {children}
-        <Toaster position="top-right" />
+          {children}
+          <Toaster position='top-right' />
         </CartProvider>
       </body>
     </html>
-  )
+  );
 }
