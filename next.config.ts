c8972dcde1f8061243a88729802914a4b5e0d422
Samin Sharar <samin@valerianfunds.com>
2026-01-31 00:21:42 +0600

    :bug: Unoptimzed image vercel bug fix

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    unoptimized: true, // ✅ disable image optimization
     remotePatterns: [
       {
         protocol: "https",
